437e7d518d11fe71e95f3f029f8a22e9ebb29608
Cherry Ng <cherry92@gmail.com>
2015-07-14 07:38:52 +0800

    async version of rpath

--- index.js
+++ index.js
@@ -7,16 +7,55 @@ var resolve = function (paths) {
     return paths.reduce(function (ps, p) {
         return ps.concat([path.resolve(p)]);
     }, []);
-}
+};
 
-var rpath = function rpath(paths, isFollowSymlink, callback) {};
+var rpath = function rpath(paths, isFollowSymlink, callback) {
+    paths = resolve(typeof paths === 'string' ? [paths] : paths);
+    isFollowSymlink = !!isFollowSymlink;
 
-rpath.sync = function rpathSync(paths, isFollowSymlink) {
-    if (typeof paths === 'string') {
-        paths = [paths];
+    var incomplete = paths.length;
+    var allPaths = [];
+    var complete = function complete (pathsFound) {
+        incomplete--;
+        allPaths = allPaths.concat(pathsFound);
+        if (incomplete === 0) {
+            callback(allPaths);
+        }
+    };
+
+    if (paths.length === 0) {
+        callback([]);
+        return;
     }
 
-    paths = resolve(paths);
+    paths.map(function (p) {
+        fs.lstat(p, function (err, stat) {
+            if (stat.isSymbolicLink() && !isFollowSymlink) {
+                complete([]);
+            }
+
+            if (stat.isFile()) {
+                complete([p]);
+            }
+
+            if (stat.isDirectory()) {
+                fs.readdir(p, function (err, subPaths) {
+                    subPaths = subPaths.map(function (sp) {
+                        return p + path.sep + sp;
+                    });
+                    rpath(subPaths, isFollowSymlink, function (allSubPaths) {
+                        complete(allSubPaths);
+                    });
+                })
+            }
+        });
+    });
+};
+
+rpath.sync = function rpathSync(paths, isFollowSymlink) {
+    paths = resolve(typeof paths === 'string' ? [paths] : paths);
+    isFollowSymlink = !!isFollowSymlink;
+
     return paths.map(function (p) {
         var stat = fs.lstatSync(p);
 
